91e20bab7be62894ee147bf917f13fb43c20d332
timschlegel <mail@timschlegel.com>
2021-03-06 14:09:41 +0100

    (Samuel) ANFORDERUNG: Replace Temp with Query (102.4); getCharge und getFrequentRenterPoint hochziehen

--- src/main/java/dhbw/fowler1/videostore/Movie.java
+++ src/main/java/dhbw/fowler1/videostore/Movie.java
@@ -24,4 +24,35 @@ public class Movie {
     public String getTitle() {
         return _title;
     }
+
+    public double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
+
+    public int frequentRenterPoints(int daysRented) {
+        int result = 1;
+
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+            result++;
+
+        return result;
+    }
+
+
 }
